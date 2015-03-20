f579815bfe72d05d196bf88311388f0b7b85860f
Henry Wallace <henry_wallace_leanna@brown.edu>
2015-03-20 02:38:30 -0400

    changed pics

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ from random import choice
 from numpy import arange
 from numpy.random import normal
 
-rules = {'1': '11', '0': lambda: choice(('1[0[0]]0', '0[0]'))}
+rules = {'1': '11', '0': lambda: choice(('1[0[0]]0', '1[0[0[0]]]'))}
 seq = eval_rules(rules, niter=4, init='0')
 draw_rules = {'1': lambda **kw: kw['t'].forward(kw['step']), 
 	'[': lambda **kw: kw['t'].left(normal(kw['depth']*0.1, 20)), \
